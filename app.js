0a6db7d68c7ff267f2c706553feb5fa787762e70
Darmaabazar <bazaracer4@gmail.com>
2024-01-29 16:51:28 +0800

    roll-dice button event listener creat

--- app.js
+++ app.js
@@ -8,12 +8,46 @@ var scores = [0, 0];
 var currentScore = 0;
 
 // шооны утгыг хадгалах хувьсагч
-var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+// document.querySelector("#score-0").textContent = 0;
+// document.querySelector("#score-1").textContent = 0;
+// document.querySelector("#current-0").textContent = 0;
+// document.querySelector("#current-1").textContent = 0;
 
-// document.querySelector(".dice").style.display = "none"; 
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+
 // document.querySelector(".player-name").innerHTML = "<em>player</em>"; 
+
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+
+// roll-dice event listener
+document.querySelector(".btn-roll").addEventListener("click", function() {
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+    diceDom.style.display = "block";
+
+    diceDom.src = "dice-" + diceNumber + ".png";
+
+    if(diceNumber !== 1) {
+        currentScore += diceNumber;
+
+        document.getElementById("current-" + activePlayer).textContent = currentScore;
+    } else {
+        diceDom.style.display = "none";
+
+        currentScore = 0;
+
+        document.getElementById("current-" + activePlayer).textContent = 0;
+
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+        document.querySelector(".player-0-panel").classList.toggle("active");
+        document.querySelector(".player-1-panel").classList.toggle("active");
+
+
+    }
+})
