a88d76e2fcd4256395c8958605499a14fc59c55c
Darmaabazar <bazaracer4@gmail.com>
2024-01-30 14:37:14 +0800

    project duuslaa

--- app.js
+++ app.js
@@ -1,53 +1,100 @@
-// idewhtei toglogchiig hadgalah huwisagch, 0 or 1
-var activePlayer = 0;  
+var activePlayer;
+var scores;
+var currentScore;
+var isNewGame;
 
-// тоглогчдын цуглуулсан оноог хадгалах хувьсагч
-var scores = [0, 0];
-
-// тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
-var currentScore = 0;
-
-// шооны утгыг хадгалах хувьсагч
-
-// document.querySelector("#score-0").textContent = 0;
-// document.querySelector("#score-1").textContent = 0;
-// document.querySelector("#current-0").textContent = 0;
-// document.querySelector("#current-1").textContent = 0;
-
-document.getElementById("score-0").textContent = 0;
-document.getElementById("score-1").textContent = 0;
-document.getElementById("current-0").textContent = 0;
-document.getElementById("current-1").textContent = 0;
-
-// document.querySelector(".player-name").innerHTML = "<em>player</em>"; 
-
+// dice image search query
 var diceDom = document.querySelector(".dice");
-diceDom.style.display = "none";
+
+// game reset
+function initGame() {
+    isNewGame = true;
+
+    // idewhtei toglogchiig hadgalah huwisagch, 0 or 1
+    activePlayer = 0;  
+
+    // тоглогчдын цуглуулсан оноог хадгалах хувьсагч
+    scores = [0, 0];
+
+    // тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+    currentScore = 0;
+
+    document.getElementById("score-0").textContent = 0;
+    document.getElementById("score-1").textContent = 0;
+    document.getElementById("current-0").textContent = 0;
+    document.getElementById("current-1").textContent = 0;
+
+    document.getElementById("name-0").textContent = "PLAYER 1";
+    document.getElementById("name-1").textContent = "PLAYER 2";
+
+    document.querySelector(".player-0-panel").classList.remove("winner");
+    document.querySelector(".player-1-panel").classList.remove("winner");
+
+    document.querySelector(".player-0-panel").classList.add("active");
+
+    diceDom.style.display = "none";
+}
+
+initGame();
 
 // roll-dice event listener
 document.querySelector(".btn-roll").addEventListener("click", function() {
-    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    if(isNewGame) {
+        var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-    diceDom.style.display = "block";
+        diceDom.style.display = "block";
 
-    diceDom.src = "dice-" + diceNumber + ".png";
+        diceDom.src = "dice-" + diceNumber + ".png";
 
-    if(diceNumber !== 1) {
-        currentScore += diceNumber;
+        if(diceNumber !== 1) {
+            currentScore += diceNumber;
 
-        document.getElementById("current-" + activePlayer).textContent = currentScore;
+            document.getElementById("current-" + activePlayer).textContent = currentScore;
+        } else {
+            switchToNextPlayer();
+        }
     } else {
-        diceDom.style.display = "none";
-
-        currentScore = 0;
-
-        document.getElementById("current-" + activePlayer).textContent = 0;
-
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-
-        document.querySelector(".player-0-panel").classList.toggle("active");
-        document.querySelector(".player-1-panel").classList.toggle("active");
-
-
+        alert("Game Over!");
     }
-})
+    
+});
+
+// hold button event listener
+document.querySelector(".btn-hold").addEventListener("click", function() {
+    if(isNewGame) {
+        scores[activePlayer] += currentScore;
+
+        document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
+    
+        
+        
+        if (scores[activePlayer] >= 100) {
+            isNewGame = false;
+            document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
+            document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
+            document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+        } else {
+            switchToNextPlayer();
+        }
+    } else {
+        alert("Game Over!");
+    }
+
+});
+
+// new game buttun event listener
+document.querySelector(".btn-new").addEventListener("click", initGame);
+
+// тоглогчийн ээлжиийг солих функц
+function switchToNextPlayer() {
+    diceDom.style.display = "none";
+
+    currentScore = 0;
+
+    document.getElementById("current-" + activePlayer).textContent = 0;
+
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+}
